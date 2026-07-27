81a264d6837785590ef8fe213dc01d068247645d
Mẫn Henry <tvm1924@gmail.com>
2026-07-27 19:59:24 +0700

    Push new code

--- script.js
+++ script.js
@@ -1113,7 +1113,6 @@ if (chatToggle && chatWidget) {
         }
       });
     });
-
   }
 }
 
@@ -1127,7 +1126,6 @@ if (typeof gsap !== 'undefined' && typeof ScrollTrigger !== 'undefined' && !pref
   const heroTl = gsap.timeline();
   heroTl.from(".hero-copy > *", {
     y: 30,
-    opacity: 0,
     duration: 0.8,
     stagger: 0.15,
     ease: "power3.out",
@@ -1135,7 +1133,6 @@ if (typeof gsap !== 'undefined' && typeof ScrollTrigger !== 'undefined' && !pref
   })
   .from(".hero-visual", {
     scale: 1.05,
-    opacity: 0,
     duration: 1.2,
     ease: "power2.out",
     clearProps: "all"
