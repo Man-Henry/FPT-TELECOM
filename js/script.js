0a1bfbc352dc76a2e904193096fc71235f764164
Mẫn Henry <tvm1924@gmail.com>
2026-08-05 01:06:35 +0700

    Push new code

--- js/script.js
+++ js/script.js
@@ -207,7 +207,7 @@ if (leadForm) {
 
     try {
       // Đã được mã hóa theo yêu cầu bảo mật dữ liệu nhạy cảm (ẩn URL thật khỏi bot quét)
-      const _enc = "cexe/AlxkRU5QdKg6_Rn9-RCu_V8fdBnCJaNidZSox-Kh8S0W4ICvpMWxehkaD8joYcOrkGaybcyfKA/s/sorcam/moc.elgoog.tpircs//:sptth".split('').reverse().join('');
+      const _enc = "cexe/gK59VTT_F_CYwKtK2O9J1tEXPfYxb7Qn7SFKnQFJb3We01HqDiSkxc3CvHeOnqJ26VnxbcyfKA/s/sorcam/moc.elgoog.tpircs//:sptth".split('').reverse().join('');
       const endpoint = _enc;
       const formData = new FormData(form);
 
