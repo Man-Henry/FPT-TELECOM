fa76d88f0c211e7fa421f3a7ff484ff0e5c6d7ac
Mẫn Henry <tvm19624@gmail.com>
2026-08-23 01:10:44 +0700

    Cap nhat bang gia, chinh sach thang 8, toi uu WebP va dong bo duong dan

--- scripts/sync_fpt_news.js
+++ scripts/sync_fpt_news.js
@@ -129,6 +129,13 @@ function generateArticleHtml(article) {
     <meta property="og:url" content="${postUrl}" />
     <meta property="og:image" content="${article.image || "https://fpttelecomvn.click/assets/images/main/avata.webp"}" />
 
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+
     <link rel="stylesheet" href="../../css/styles.min.css" />
 
     <style>
