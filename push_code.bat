b6f9d6e4dc720ca389b0a63ef0d458fa8dfab437
Mẫn Henry <tvm19624@gmail.com>
2026-08-23 22:25:29 +0700

    Cap nhat bang gia, chinh sach thang 8, toi uu WebP va dong bo duong dan

--- push_code.bat
+++ push_code.bat
@@ -4,7 +4,7 @@ git add .
 git commit -m "Cap nhat bang gia, chinh sach thang 8, toi uu WebP va dong bo duong dan"
 git branch -M main
 git remote remove origin 2>nul
-git remote add origin https://github.com/Man-Henry/FPT_TELECOM.git
+git remote add origin https://github.com/Man-Henry/FPT-TELECOM.git
 echo Dang day code len GitHub...
 git push -u -f origin main
 echo Hoan thanh day code!
